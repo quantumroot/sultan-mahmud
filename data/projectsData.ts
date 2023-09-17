7068cc3f4c55d598f32753c7f45d7f836136a523
tmp.faisal-ahmed <tmp.faisal-ahmed@cafu.com>
2023-09-17 17:13:08 +0600

    begin meditations

---
+++ data/projectsData.ts
@@ -0,0 +1,15 @@
+const projectsData = [
+  {
+    title: 'Shuhashini (Who smiles beautifully, It is a metaphor, not a real person)',
+    description: `Does love's contemplation only have to revolve around the person we love? Or can love's thoughts inspire a person to become philosophical, to think? Can it teach a person to question? To challenge societal norms? Can it make one ponder about the existence of this universe, the reason for existence, about the Creator? Can love's thoughts not turn a person into a warrior of justice? Can love's contemplation not empower one to stand firm against the injustices of society?
+
+    Must poems of love always be in intricate language? Can't there be love poems in the language of our farmers and laborers? Can't there be poetry in the scent of the soil of our motherland, Bengal? Does reading poetry always have to be a heavy experience? In a casual evening tea gathering, can there not be simple poems just for a laugh, just for love?
+    
+    The poems in your book attempt to address these questions, taking on a thought-provoking journey. Yet, in this busy city life, if these poems can make you smile a bit and think a bit, then the poet's work is accomplished
+    for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.rokomari.com/book/332506',
+  },
+]
+
+export default projectsData
