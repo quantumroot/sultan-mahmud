038f11502ec03922717fc2042f5179ca07bb4ff8
tmp.faisal-ahmed <tmp.faisal-ahmed@cafu.com>
2023-09-17 17:31:50 +0600

    fix: social-banner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -6,10 +6,10 @@ const siteMetadata = {
   description: 'Intellectual Journey of a Meditator',
   language: 'en-us',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://faisal-meditates.vercel.app',
+  siteRepo: 'https://github.com/quantumroot/meditations',
   siteLogo: '/static/images/logo.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
   email: 'address@yoursite.com',
   github: 'https://github.com/quantumroot',
